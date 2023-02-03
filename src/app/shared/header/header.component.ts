967ab01707ff42fb16b3859d2bd8da996c0b3ca2
Karim <karim@blasksmith.studio>
2023-02-03 17:23:25 +0400

    refactor(Header): display goback conditionnaly

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { filter, map, Observable, of } from 'rxjs';
+import { Router, NavigationStart } from '@angular/router';
 @Component({
   selector: 'telesport-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public displayGoBack$: Observable<boolean>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.displayGoBack$ = of(false);
   }
 
+  ngOnInit(): void {
+    this.displayGoBack$ = this.router.events
+      .pipe(
+        filter(routerEvent => routerEvent instanceof NavigationStart),
+        map(routerEvent => (routerEvent as NavigationStart).url !== '/')
+      )
+  }
 }
