7dcd84a7d82d19e408ca761534683b157db34c80
VelosoTony <veloso.tony@gmail.com>
2023-02-17 14:53:48 +0100

    define interface models

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,4 +1,7 @@
 // TODO: create here a typescript interface for an olympic country
+
+import { Participation } from "./Participation";
+
 /*
 example of an olympic country:
 {
@@ -7,3 +10,10 @@ example of an olympic country:
     participations: []
 }
 */
+export interface Olympic {
+  id: number;
+  country: string;
+  participations: Participation[];
+}
+
+
--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -9,3 +9,10 @@ example of participation:
     athleteCount: 372
 }
 */
+export interface Participation {
+  id: number;
+  year: number;
+  city: string;
+  medalsCount: number;
+  athleteCount: number;
+}
