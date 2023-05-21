c002389df779acafa6b4f12ea3d0fd35a25dbc48
alexia pratensi <alexiapratensi@hotmail.fr>
2023-05-21 16:34:19 +0200

    css detailcomponent ok

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -18,26 +18,30 @@ export class DetailComponent implements OnInit, OnDestroy {
   public subscription: Subscription = new Subscription();
   public olympicData!: {"name": string, "series": {"name": string, "value": number}[]} [];
   public series: {"name": string, "value": number}[]  = [];
+  public nameOfCountry!: string;
+  public numberOfAthletes!: number;
+  public numberOfMedals!: number;
+  public numberOfEntries!: number;
 
 
 
 
-  view: any = [700, 300];
+  view: any = [750, 350];
 
   // options
-  legend: boolean = true;
+  legend: boolean = false;
   showLabels: boolean = true;
   animations: boolean = true;
   xAxis: boolean = true;
   yAxis: boolean = true;
   showYAxisLabel: boolean = true;
   showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Year';
+  xAxisLabel: string = 'Dates';
   yAxisLabel: string = 'Medals';
   timeline: boolean = true;
 
   colorScheme: any = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+    domain: ['#5F264A','#850E35', '#a48da6','#93BFCF' ]
   };
 
   constructor(private olympicService: OlympicService, private router: Router, private route: ActivatedRoute) {}
@@ -50,11 +54,22 @@ export class DetailComponent implements OnInit, OnDestroy {
     this.subscription = this.olympicService.getOlympics().subscribe(
       olympics => {
 
+
         const olympicFound = olympics.find((olympic) => olympic.country === olympicCountry)
         console.log('Olympic found dans avant le if:', olympicFound);
 
+
         if (olympicFound !== undefined) {
-          console.log('Olympic found dans le if:', olympicFound);
+          // title with name of country
+          this.nameOfCountry = olympicFound.country;
+          //Total number of athletes
+          this.numberOfAthletes = olympicFound.participations.reduce((totalAthletes, participation) => totalAthletes + participation.athleteCount, 0);
+          // Total number of medals
+          this.numberOfMedals = olympicFound.participations.reduce((totalMedals, participation) => totalMedals + participation.medalsCount, 0)
+          // Total number of entries
+          this.numberOfEntries = olympicFound.participations.length
+
+          //line Chart
            this.series = [];
 
             olympicFound.participations.forEach((participation) => {
