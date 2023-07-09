84aa7ae5f1840e43c8e72b953a4c8c43c8b620ac
rayanadir <rayan.dahmena@gmail.com>
2023-07-09 13:06:47 +0200

    get data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Olympic } from 'src/app/core/models/Olympic';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,20 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<Olympic[]> = of([]);
+  public olympics : Olympic[] = []
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$ = this.olympicService.getOlympics$()
+
+    this.olympicService.getOlympics().subscribe((data) => {
+      this.olympics=data.map((olympic) => {
+        return olympic
+      })
+    });
+    
   }
+
 }
