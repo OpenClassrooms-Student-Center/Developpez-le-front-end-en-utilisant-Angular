f35c0874580c47c78be6a051e0ef7a96fecab105
rayanadir <rayan.dahmena@gmail.com>
2023-07-11 17:16:38 +0200

    add breakpointChanged method

--- src/app/core/services/responsive.service.ts
+++ src/app/core/services/responsive.service.ts
@@ -8,10 +8,23 @@ import { Observable } from 'rxjs';
 })
 export class ResponsiveService {
 
+  currentBreakpoint: any= '';
+
   constructor(private responsive: BreakpointObserver) { }
 
-  observeBreakpoint(): Observable<BreakpointState> {
+  observeBreakpoint() {
     const {  Web,  Tablet,  Handset } = Breakpoints;
     return this.responsive.observe([Web,  Tablet,  Handset ])
+  }
+
+  breakpointChanged() : "desktop" | "tablet" | "phone" | undefined {
+    if (this.responsive.isMatched(Breakpoints.Web)) {
+      return this.currentBreakpoint = "desktop";
+    } else if (this.responsive.isMatched(Breakpoints.Tablet)) {
+      return this.currentBreakpoint = "tablet";
+    } else if (this.responsive.isMatched(Breakpoints.Handset)) {
+      return this.currentBreakpoint = "phone";
+    }
+    return undefined;
   }
 }
