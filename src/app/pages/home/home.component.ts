043ff9475bacd8b7bf6c4f90035694a1486adadf
rayanadir <rayan.dahmena@gmail.com>
2023-07-11 23:35:32 +0200

    format countries data + add pie chart variables

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { ResponsiveService } from 'src/app/core/services/responsive.service';
+import { ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -10,20 +11,47 @@ import { ResponsiveService } from 'src/app/core/services/responsive.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+
   public olympics$: Observable<Olympic[]> = of([]);
   public olympics: Olympic[] = [];
 
+
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+  }
+  public pieChartCountries: Array<string> = [];
+  public countriesMedals: Array<number> = [];
+  public pieChartDatasets : Array<{data: Array<number>}>= [];
+  public pieChartLegend = false;
+  public pieChartPlugins = [];
+
   currentBreakpoint:"desktop" | "tablet" | "phone" | undefined;
 
-  constructor(private olympicService: OlympicService, private responsiveService: ResponsiveService) { }
+
+
+  constructor(
+     private olympicService: OlympicService,
+     private responsiveService: ResponsiveService
+  ) { }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics$()
 
     this.olympicService.getOlympics().subscribe((olympicData) => {
       this.olympics = olympicData.map((olympic) => {
-        return olympic
+        return olympic;
       })
+      
+      if(this.olympics.length>0) {
+        this.pieChartCountries = this.olympics.map((country) => { return country.country })
+        this.olympics.map((medals) => {
+          const medalsParticipations = medals.participations.map((participations) => {
+            return participations.medalsCount;
+          }).reduce((prev,curr) => prev+curr);
+          this.countriesMedals.push(medalsParticipations)
+        })
+        this.pieChartDatasets = [{ data: this.countriesMedals }]
+      }
     });
 
     /**
