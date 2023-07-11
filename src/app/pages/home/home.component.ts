975fd3f78fc81c4dbdb2bc90fa5c691b2df059b5
rayanadir <rayan.dahmena@gmail.com>
2023-07-11 17:19:49 +0200

    observe current screen format

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { ResponsiveService } from 'src/app/core/services/responsive.service';
 
 @Component({
   selector: 'app-home',
@@ -10,20 +11,27 @@ import { Olympic } from 'src/app/core/models/Olympic';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[]> = of([]);
-  public olympics : Olympic[] = []
+  public olympics: Olympic[] = [];
 
-  constructor(private olympicService: OlympicService) {}
+  currentBreakpoint:"desktop" | "tablet" | "phone" | undefined;
+
+  constructor(private olympicService: OlympicService, private responsiveService: ResponsiveService) { }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics$()
 
     this.olympicService.getOlympics().subscribe((olympicData) => {
-      this.olympics=olympicData.map((olympic) => {
+      this.olympics = olympicData.map((olympic) => {
         return olympic
       })
     });
-    
-    
+
+    /**
+     * Observe current window format : "desktop" | "tablet" | "phone" | undefined
+     */
+    this.responsiveService.observeBreakpoint().subscribe(() => {
+      this.currentBreakpoint = this.responsiveService.breakpointChanged();
+    });
   }
 
 }
