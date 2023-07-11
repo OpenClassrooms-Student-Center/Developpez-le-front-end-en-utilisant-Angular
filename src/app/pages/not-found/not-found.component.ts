c08614f86b3a7d25cb61e7178cfaf3f456017360
rayanadir <rayan.dahmena@gmail.com>
2023-07-11 17:20:51 +0200

    observe current screen format

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResponsiveService } from 'src/app/core/services/responsive.service';
 
 @Component({
   selector: 'app-not-found',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  currentBreakpoint: "desktop" | "tablet" | "phone" | undefined;
+
+  constructor(private responsiveService: ResponsiveService) { }
 
   ngOnInit(): void {
+    /**
+    * Observe current window format : "desktop" | "tablet" | "phone" | undefined
+    */
+    this.responsiveService.observeBreakpoint().subscribe(() => {
+      this.currentBreakpoint = this.responsiveService.breakpointChanged();
+    });
   }
 
 }
