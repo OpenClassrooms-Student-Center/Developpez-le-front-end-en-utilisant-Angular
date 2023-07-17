212a8fa4f4b386216402a95ee89522fa4f2a570a
rayanadir <rayan.dahmena@gmail.com>
2023-07-18 00:02:36 +0200

    get country details

---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic';
+import { Participation } from 'src/app/core/models/Participation';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+  
+  public olympics$: Observable<Olympic[]> = of([]);
+  public olympics: Olympic[] = [];
+  public country!: Olympic | undefined;
+  public medals: number | undefined = 0;
+  public athletes: number | undefined = 0;
+
+  constructor(private route: ActivatedRoute, private router: Router, private olympicService: OlympicService){
+
+  }
+
+  ngOnInit() {
+      let id = parseInt(this.route.snapshot.params['id']);
+      this.olympicService.getOlympics().subscribe((olympicData) => {
+        if(olympicData.length>0){
+          this.country = olympicData.find(country => country.id==id);
+          this.medals= this.country?.participations.map((participation) => {
+            return participation.medalsCount
+          }).reduce((prev,curr) => prev+curr);
+          
+          this.athletes=this.country?.participations.map((participation) => {
+            return participation.athleteCount
+          }).reduce((prev,curr) => prev+curr)
+        }
+
+        
+      })
+  }
+}
