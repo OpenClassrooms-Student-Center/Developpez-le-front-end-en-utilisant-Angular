ccb104b6af14b0caa2f57175109685e61e71bf4e
rayanadir <rayan.dahmena@gmail.com>
2023-07-18 23:26:37 +0200

    subscribe router events + navigateBack method

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  showBackArrow: boolean = false;
+
+  constructor(public router: Router, public route: ActivatedRoute){
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd){
+        this.showBackArrow=event.url.includes('/details/');        
+      }
+    })
+    
+  }
+
+  ngOnInit() {   
+    
+  }
+
+  navigateBack(){
+    this.router.navigateByUrl('/home')
+  }
 
 }
