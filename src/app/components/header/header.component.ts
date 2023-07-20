6d7834c6b4c1df8d5dff0e255fe6042efadff18d
rayanadir <rayan.dahmena@gmail.com>
2023-07-20 20:48:03 +0200

    add currentBreakpoint subscribing + add comments

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { ResponsiveService } from 'src/app/core/services/responsive.service';
 
 @Component({
   selector: 'app-header',
@@ -8,22 +9,35 @@ import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  currentBreakpoint: "desktop" | "tablet" | "phone" | undefined;
   showBackArrow: boolean = false;
 
-  constructor(public router: Router, public route: ActivatedRoute){
+  constructor(public router: Router, public route: ActivatedRoute, private responsiveService: ResponsiveService) {
+    /**
+     * Subscribe to current url path, if it includes '/details/' (located on details page),
+     * then showBackArrow is true and shows the back arrow in the header to navigate back
+     */
     this.router.events.subscribe((event) => {
-      if(event instanceof NavigationEnd){
-        this.showBackArrow=event.url.includes('/details/');        
+      if (event instanceof NavigationEnd) {
+        this.showBackArrow = event.url.includes('/details/');
       }
     })
-    
+
   }
 
-  ngOnInit() {   
-    
+  ngOnInit() {
+    /**
+    * Observe current window format : "desktop" | "tablet" | "phone" | undefined
+    */
+    this.responsiveService.observeBreakpoint().subscribe(() => {
+      this.currentBreakpoint = this.responsiveService.breakpointChanged();
+    });
   }
 
-  navigateBack(){
+  /**
+   * Navigate to home
+   */
+  navigateBack() {
     this.router.navigateByUrl('/home')
   }
 
