63c7a2f4f0e5afd2bdcccd1a0e1e6f6ae726542e
rayanadir <rayan.dahmena@gmail.com>
2023-07-22 00:39:54 +0200

    add data methods

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -11,15 +11,14 @@ export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
   private olympics$: BehaviorSubject<Olympic[]> = new BehaviorSubject<Olympic[]>([]);
   public errors: string[] = [];
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   loadInitialData(): Observable<Olympic[]> {
     return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
+        // push errors to errors array
         this.errors.push(error);
-        console.error(error);
         // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next([]);
         return caught;
@@ -27,13 +26,39 @@ export class OlympicService {
     );
   }
 
-  getOlympics$() : Observable<Olympic[]> {
-    return this.olympics$.asObservable(); 
+  /**
+   * Get olympics data as observable
+   * @returns Observable of Olympic array
+   */
+  getOlympics$(): Observable<Olympic[]> {
+    return this.olympics$.asObservable();
   }
-  
+
+  /**
+   * Get olympics data of observable with a delay of 0.5s
+   * @returns Observable of Olympic array
+   */
   getOlympics(): Observable<Olympic[]> {
     return this.olympics$.asObservable().pipe(delay(500))
-    
+  }
+
+  /**
+   * Get a country data by its id
+   * @param id the id of the country
+   * @returns 
+   */
+  getCountry(id: number): Observable<Olympic | undefined> {
+    return this.olympics$.asObservable().pipe(map((countries) => {
+        return countries.find(country => country.id == id);
+    }))
+  }
+
+  /**
+   * Get errors from loadInitialData method
+   * @returns Array of string
+   */
+  getErrors(): string[] {
+    return this.errors;
   }
 
 }
