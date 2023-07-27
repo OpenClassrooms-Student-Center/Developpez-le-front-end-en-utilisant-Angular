6163d200a493b7e575d62b0bf77b3081de2cf322
rayanadir <rayan.dahmena@gmail.com>
2023-07-27 19:39:47 +0200

    add country verification

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Observable, Subject, delay, of, takeUntil } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { ResponsiveService } from 'src/app/core/services/responsive.service';
@@ -12,15 +12,14 @@ import { ResponsiveService } from 'src/app/core/services/responsive.service';
 })
 export class DetailsComponent implements OnInit, OnDestroy {
 
-  destroyed = new Subject<void>()
-  public olympics$: Observable<Olympic[]> = of([]);
-  public olympics: Olympic[] = [];
-  public country: Olympic | undefined;
+  public countryDataSubscription!: Subscription;
+  public countryCheckSubscription!: Subscription;
+  public lineChartSubscription!:Subscription
   public medals: number | undefined = 0;
   public athletes: number | undefined = 0;
   public loaded: boolean = false;
-  currentBreakpoint:"desktop" | "tablet" | "phone" | undefined;
-
+  currentBreakpoint: "desktop" | "tablet" | "phone" | undefined;
+  public country: Olympic | undefined = <Olympic>({ country: "", id: 0, participations: [] })
   public countryInfo: Array<
     {
       name: string | undefined,
@@ -46,19 +45,19 @@ export class DetailsComponent implements OnInit, OnDestroy {
   timeline: boolean = false;
   autoScale: boolean = true;
 
-  width:number=800;
-  height:number=600;
-  view:[number,number]=[this.width,this.height]
+  width: number = 800;
+  height: number = 600;
+  view: [number, number] = [this.width, this.height]
 
 
   constructor(private route: ActivatedRoute, private router: Router, private olympicService: OlympicService, private responsiveService: ResponsiveService) {
     /**
      * Get width and height values to set the line chart dimensions (this.view variable) 
      */
-    this.width= innerWidth / 1.35;
-    this.height=this.width/1.3;
-    if(this.width>800) this.width=800
-    if(this.height>600) this.height=600
+    this.width = innerWidth / 1.35;
+    this.height = this.width / 1.3;
+    if (this.width > 800) this.width = 800
+    if (this.height > 600) this.height = 600
     this.view = [this.width, this.height];
   }
 
@@ -66,45 +65,44 @@ export class DetailsComponent implements OnInit, OnDestroy {
     // get the params id
     let id = parseInt(this.route.snapshot.params['id']);
 
-    // get all countries 
-    this.olympicService.getOlympics().pipe(takeUntil(this.destroyed)).subscribe((olympicData) => {
-      if (olympicData.length > 0) {
-        // find country by id
-        this.country = olympicData.find(country => country.id == id);
-        this.loaded = true;
+    this.countryCheckSubscription = this.olympicService.checkCountry(id).subscribe((res) => {
 
-        // If no country found, redirect to error page
-        if (this.country == undefined) {
-          this.router.navigateByUrl('**')
-        } else {
-          
+      // Country not found, redirection to error page
+      if (res == 'not_found' && res != undefined) {
+        this.router.navigateByUrl('**');
+      }
+      // Country found, get data
+      else if (res != undefined) {
+       this.countryDataSubscription= this.olympicService.getCountry(id).subscribe((country) => {
+          this.loaded=true;
+          this.country=country;
           // count all country medals
-          this.medals = this.country.participations.map((participation) => {
-            return participation.medalsCount
+          this.medals = this.country?.participations.map((participation) => {
+            return participation.medalsCount;
           }).reduce((prev, curr) => prev + curr);
 
           // count all country athletes
-          this.athletes = this.country.participations.map((participation) => {
+          this.athletes = this.country?.participations.map((participation) => {
             return participation.athleteCount
           }).reduce((prev, curr) => prev + curr);
 
-
           /**
-           * Get min and max medals to set the y scale
-           */
-          const medalsCount: number[] = this.country.participations.map((p) => {return p.medalsCount})
-          let max = Math.max(...medalsCount);
-          let min = Math.min(...medalsCount);
+          * Get min and max medals to set the y scale
+          */
+          const medalsCount: number[] | undefined = country?.participations.map((p) => { return p.medalsCount })
+          let max = Math.max(...medalsCount!);
+          let min = Math.min(...medalsCount!);          
           let delta = Math.floor(max - min);
-          this.yScaleMin = Math.floor(min - delta*0.2);
-          this.yScaleMax =Math.floor(max + delta*0.2)
+          this.yScaleMin = Math.floor(min - delta * 0.3);
+          this.yScaleMax = Math.floor(max + delta * 0.3);
+          
 
           /**
            * Expected series data format : array of objects 
            * value key : number (total of country medals won)
            * name key : string (participation year)
            */
-          const series = this.country.participations.map((participation) => {
+          const series = this.country?.participations.map((participation) => {
             return {
               value: participation.medalsCount,
               name: participation.year.toString()
@@ -116,32 +114,29 @@ export class DetailsComponent implements OnInit, OnDestroy {
            * name key : string (name of the country)
            * series key : array of objects 
            */
-          const country = {
-            name: this.country.country,
+          const countryObject = {
+            name: this.country?.country,
             series,
           }
           // Single country object expected because we want single country
-          this.countryInfo.push(country);
-        }
+          this.countryInfo= [countryObject]
+        })
       }
-    })
-
-
+    });
 
     /**
      * Observe current window format : "desktop" | "tablet" | "phone" | undefined
      */
-    this.responsiveService.observeBreakpoint().pipe(takeUntil(this.destroyed)).subscribe(() => {
+    this.lineChartSubscription= this.responsiveService.observeBreakpoint().subscribe(() => {
       this.currentBreakpoint = this.responsiveService.breakpointChanged();
-    });    
-
-
-    
+    });
   }
 
-  ngOnDestroy(): void {
-      this.destroyed.next();
-      this.destroyed.complete()
+
+  ngOnDestroy() {
+    this.lineChartSubscription.unsubscribe();
+    this.countryDataSubscription.unsubscribe();
+    this.countryCheckSubscription.unsubscribe();
   }
 
   /**
@@ -150,9 +145,9 @@ export class DetailsComponent implements OnInit, OnDestroy {
    */
   onResize(event: any) {
     this.width = event.target.innerWidth / 1.35;
-    this.height = this.width/1.3;
-    if(this.width>800) this.width=800
-    if(this.height>600) this.height=600
+    this.height = this.width / 1.3;
+    if (this.width > 800) this.width = 800
+    if (this.height > 600) this.height = 600
     this.view = [this.width, this.height];
   }
 }
