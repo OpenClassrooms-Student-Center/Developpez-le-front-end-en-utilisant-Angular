9a2882cb919c6537f2b7eb10cfb5b64dd35532e1
rayanadir <rayan.dahmena@gmail.com>
2023-07-31 23:15:44 +0200

    remove unused package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HeaderComponent } from './components/header/header.component';
-import { NgChartsModule } from 'ng2-charts';
+//import { NgChartsModule } from 'ng2-charts';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DetailsComponent } from './pages/details/details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, HeaderComponent, DetailsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgChartsModule, NgxChartsModule,BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule,BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
