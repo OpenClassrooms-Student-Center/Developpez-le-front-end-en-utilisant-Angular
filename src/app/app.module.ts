2812766dd3bfb442694d5e8786a6c85201b73d70
rayanadir <rayan.dahmena@gmail.com>
2023-07-31 21:06:02 +0200

    delete footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { NgChartsModule } from 'ng2-charts';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DetailsComponent } from './pages/details/details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, HeaderComponent, FooterComponent, DetailsComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, HeaderComponent, DetailsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgChartsModule, NgxChartsModule,BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
