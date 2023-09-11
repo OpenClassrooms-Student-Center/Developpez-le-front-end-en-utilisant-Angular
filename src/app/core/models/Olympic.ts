b8354aa7a168c7285ae1bc436941fc9caa884b6c
paulmarniquet <paul.marniquet@epitech.eu>
2023-09-11 12:00:53 +0200

    Understanding the projet, added type for Olympic and Participation

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,7 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
+export class Olympic {
+  constructor(
+    id: number,
+    country: string,
+    participations: [],
+  ) {}
 }
-*/
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import {Olympic} from "../../core/models/Olympic";
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,14 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<null> = of(null);
+
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
   }
+
+
 }
