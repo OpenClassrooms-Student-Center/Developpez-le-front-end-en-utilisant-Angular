10620a7aaf82c2ac731cb82079f7cf674bab4fa2
paulmarniquet <paul.marniquet@epitech.eu>
2023-09-16 17:06:28 +0200

    Project done

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -20,10 +20,8 @@ export class OlympicService {
         this.olympics$.next(value);
       }),
       catchError((error, caught) => {
-        // TODO: improve error handling
         console.error(error);
         this.olympics$.error(error);
-        // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next(null);
         return caught;
       })
