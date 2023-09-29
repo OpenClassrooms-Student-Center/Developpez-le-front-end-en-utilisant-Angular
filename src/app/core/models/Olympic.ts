ee2a4879361e222e3594846d166416914e2c6694
Mathieu Hallez <mathew.hallez@gmail.com>
2023-09-29 22:45:36 +0200

    Create to class type

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,11 +1,28 @@
-import { IParticipation } from "./Participation";
+import { DtrParticipation, Participation } from "./Participation";
 
-export type Olympic = IOlympic | undefined | null;
+export type DtrOlympic = IOlympic | undefined | null;
 
-export type Olympics = Array<Olympic> | undefined | null;
+export type Olympics = Array<DtrOlympic> | undefined | null;
 
 interface IOlympic {
     id : number,
     country : string,
-    participations : Array<IParticipation>
+    participations : Array<DtrParticipation>
 };
+
+export class Olympic {
+
+    id : number;
+    country : string;
+    participations : Array<Participation>;
+
+    constructor(
+        $olympic : DtrOlympic
+    ) {
+        this.id = $olympic!.id;
+        this.country = $olympic!.country;
+        this.participations = $olympic!.participations.map((value : DtrParticipation) => new Participation(value));
+    }
+
+
+}
--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -1,7 +1,28 @@
-export interface IParticipation {
+export type DtrParticipation = IParticipation;
+
+interface IParticipation {
     id : number,
     year : number,
     city : string,
     medalsCount: number,
     athleteCount: number
 };
+
+export class Participation {
+    
+    id : number;
+    year : number;
+    city : string;
+    medalsCount: number;
+    athleteCount: number;
+
+    constructor(
+        $participation : IParticipation
+    ) {
+        this.id = $participation.id;
+        this.year = $participation.year;
+        this.city = $participation.city;
+        this.medalsCount = $participation.medalsCount;
+        this.athleteCount = $participation.athleteCount;
+    }   
+}
