9ccfcbe040f4108f22836318ad5edaa5957bfe57
Younes Lahouiti <loller763@gmail.com>
2023-10-06 12:17:31 +0200

    Creation of a first Angular component for the modal window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DialogComponent } from './components/shared/dialog/dialog/dialog.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, DialogComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/shared/dialog/dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss'],
+})
+export class DialogComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit(): void {
+    //
+  }
+
+  ngOnDestroy(): void {
+    //
+  }
+}
