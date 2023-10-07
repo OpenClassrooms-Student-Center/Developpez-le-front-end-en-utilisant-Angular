40a05c978bebfeff631fc66ccfcee684a00b6dab
Younes Lahouiti <loller763@gmail.com>
2023-10-07 11:30:21 +0200

    Translation of all the texts content into English

--- src/app/public/images/index.images.ts
+++ src/app/public/images/index.images.ts
@@ -1,3 +1,4 @@
 import SVG_URLS from './svg/svg.images';
+import PNG_URLS from './png/png.images';
 
-export { SVG_URLS };
+export { SVG_URLS, PNG_URLS };
---
+++ src/app/public/images/png/png.images.ts
@@ -0,0 +1,7 @@
+const baseUrl: string = '/assets/images/png';
+
+const PNG_URLS = {
+  banner: `${baseUrl}/telesport_banner.png`,
+};
+
+export default PNG_URLS;
