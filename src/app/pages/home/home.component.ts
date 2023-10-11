ea220f6c2310510723611c0f11ce44bff493bd90
TheoPierne <theo.pierne2@gmail.com>
2023-10-11 21:07:19 +0200

    feat: implémentation du premier graphique

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 import { Observable, of } from 'rxjs';
+import { ICountry } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -9,10 +11,38 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+  public Highcharts: typeof Highcharts = Highcharts;
+  public chartOptions: Highcharts.Options = {
+    title: {
+      text: ''
+    },
+    series: [
+      {
+        data: [],
+        type: 'pie',
+      },
+    ],
+  };
+  public updateFlag: boolean = false;
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.subscribe((countries) => {
+      if (Array.isArray(countries)) {
+        if (this.chartOptions.series) {
+          this.chartOptions.series[0] = {
+            data: countries.map((countryData: ICountry) => ({
+              name: countryData.country,
+              y: countryData.participations.reduce((acc, country) => acc + country.medalsCount, 0)
+            })),
+            type: 'pie',
+          } as Highcharts.SeriesPieOptions;
+
+          this.updateFlag = true;
+        }
+      }
+    });
   }
 }
