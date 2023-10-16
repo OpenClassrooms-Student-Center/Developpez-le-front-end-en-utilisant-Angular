bed6ffc671e6ac267a452354aaec83bdb299c97c
Mathieu Hallez <mathew.hallez@gmail.com>
2023-10-16 17:26:15 +0200

    Add Line chart

--- src/app/pages/country-participation/country-participation.component.ts
+++ src/app/pages/country-participation/country-participation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription, partition } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic/olympic.service';
 
@@ -14,6 +14,16 @@ export class CountryParticipationComponent implements OnInit {
 
   public olympic! : Olympic;
 
+  // Line Chart Options
+  showXAxisLabel : boolean = true;
+  showYAxisLabel : boolean = false;
+  xAxis : boolean = true;
+  yAxis : boolean = true;
+  xAxisLabel : string = 'Dates';
+  timeline : boolean = true;
+  autoScale : boolean = true;
+  multi : Array<lineChartData> = [];
+
   constructor(
     private olympicService: OlympicService,
     private route : ActivatedRoute,
@@ -23,10 +33,21 @@ export class CountryParticipationComponent implements OnInit {
   ngOnInit(): void {
     let id : number = +this.route.snapshot.params['id'];
     this.olympic = this.olympicService.getOlympic(id);
+    console.log('Olympic: ' + this.olympic)
+    let series : {name: string, value : number}[] = this.olympic.participations.map((partition) => ({name: String(partition.year), value: partition.medalsCount }))
+    this.multi = [{
+      name : this.olympic.country,
+      series: series
+    }]
   }
 
   goBack(): void {
     this.router.navigateByUrl('');
   }
 
-}
+};
+
+type lineChartData = {
+  name : string,
+  series: Array<{name : string, value: any}>
+};
