1049de2e2dd34f2625f6d7bdece8969a41b17e24
Younes Lahouiti <loller763@gmail.com>
2023-10-29 15:06:24 +0100

    Addition of a reusable component to reafctor the code on both pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {
   HeaderComponent,
   FooterComponent,
 } from '@components/common/layout/index.layout';
+import { InfoIndicatorsComponent } from './components/shared/info-indicators/info-indicators.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import {
     SpinLoaderComponent,
     InfoCardComponent,
     TooltipComponent,
+    InfoIndicatorsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shared/info-indicators/info-indicators.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-indicators',
+  templateUrl: './info-indicators.component.html',
+  styleUrls: ['./info-indicators.component.scss'],
+})
+export class InfoIndicatorsComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() indicators: { title: string; value: number }[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
