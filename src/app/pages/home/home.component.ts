7aaa08ddd59f38837daa0fa6258048a84478d1b3
Frederic Guyont <you@example.com>
2023-10-30 10:20:01 +0100

    ADD: First chart creation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { single } from './data';
+import { Color, ScaleType, LegendPosition } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +12,38 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
 
-  constructor(private olympicService: OlympicService) {}
+  single!: any[];
+  view: [number, number] = [700, 400];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition!: LegendPosition.Below;
+
+  colorScheme: Color = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+    group: ScaleType.Ordinal,
+    selectable: true,
+    name: 'Customer Usage'
+  };
+
+  constructor(private olympicService: OlympicService) {
+    Object.assign(this, { single });
+  }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
