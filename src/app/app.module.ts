5b707b7b4ba68922e33b8a38ac06733763562d8e
TheoPierne <theo.pierne2@gmail.com>
2023-11-03 15:31:27 +0100

    feat: Ajout barre de navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material';
+
 import { HighchartsChartModule } from 'highcharts-angular';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +15,14 @@ import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, DetailsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HighchartsChartModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    HighchartsChartModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
