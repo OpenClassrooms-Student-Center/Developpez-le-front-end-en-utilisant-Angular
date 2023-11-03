692f8d40c540f3ddb056eaeb92f98013ea540004
TheoPierne <theo.pierne2@gmail.com>
2023-11-03 13:11:12 +0100

    feat: Ajout de la route pour la nouvelle page des détails

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, DetailsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, HighchartsChartModule],
   providers: [],
   bootstrap: [AppComponent],
