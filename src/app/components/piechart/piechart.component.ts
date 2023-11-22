795a640f2241a8718f8e44b34bd461db508bb47d
julien382 <julhermain@gmail.com>
2023-11-22 21:59:25 +0100

    affichage des datas avec olympics$

--- src/app/components/piechart/piechart.component.ts
+++ src/app/components/piechart/piechart.component.ts
@@ -1,76 +1,21 @@
-import { Component,Input, OnInit } from '@angular/core';
-import { Observable, filter, of } from 'rxjs';
-import Olympic from 'src/app/core/models/Olympic';
-import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-piechart',
-  templateUrl: './piechart.component.html',
-  styleUrls: ['./piechart.component.scss']
+  template: `
+    <ngx-charts-pie-chart
+      [results]="data"
+      [view]="[400, 300]"
+      [gradient]="false"
+      (select)="onSelect($event)"
+    >
+    </ngx-charts-pie-chart>
+  `,
 })
-export class PiechartComponent implements OnInit {
-  public olympics$: Observable<Olympic[]>;
+export class PiechartComponent {
+  @Input() data: any[] = [];
 
-
-  data = [
-    {
-      "name": "Germany",
-      "value": 40632,
-      "extra": {
-        "code": "de"
-      }
-    },
-    {
-      "name": "United States",
-      "value": 50000,
-      "extra": {
-        "code": "us"
-      }
-    },
-    {
-      "name": "France",
-      "value": 36745,
-      "extra": {
-        "code": "fr"
-      }
-    },
-    {
-      "name": "United Kingdom",
-      "value": 36240,
-      "extra": {
-        "code": "uk"
-      }
-    },
-    {
-      "name": "Spain",
-      "value": 33000,
-      "extra": {
-        "code": "es"
-      }
-    },
-    {
-      "name": "Italy",
-      "value": 35800,
-      "extra": {
-        "code": "it"
-      }
-    }
-  ]; // Propriété pour les données du graphique
-  @Input() explodeSlices: boolean = false; // Propriété pour l'explosion des tranches
-  
-  // Options de configuration
-  pieChartOptions: any = {
-    // Configurations supplémentaires ici
-  };
-  
-  constructor(private olympicService: OlympicService) {
-    this.olympics$ = this.olympicService.getOlympics().pipe(filter(value => value));
-  }
-
-
-  ngOnInit(): void {
-    
-    console.log(this.olympics$)
-    // Initialisations ou manipulations de données supplémentaires si nécessaires
+  onSelect(event: any): void {
+    // Handle chart selection if needed
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,17 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
+interface Participation {
+  id: number;
+  year: number;
+  city: string;
+  medalsCount: number;
+  athleteCount: number;
+}
+
+interface Country {
+  id: number;
+  country: string;
+  participations: Participation[];
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<Country[]> = of([]);
+  public selectedYear = 2020;
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+  }
+
+  filterData(data: Country[], year: number): any[] {
+    return data.map((country) => {
+      const participation = country.participations.find((p: Participation) => p.year === year);
+      return {
+        name: country.country,
+        value: participation ? participation.medalsCount : 0,
+      };
+    });
   }
 }
