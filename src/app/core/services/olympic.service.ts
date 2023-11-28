b9922cda9400d3729828f273bf1d10e2d53234f0
julien382 <julhermain@gmail.com>
2023-11-28 12:34:40 +0100

    save visio/data load

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,18 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import Olympic from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[]>([]);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => {
         console.log(value); // Log the data in the console
         this.olympics$.next(value);
@@ -22,7 +23,7 @@ export class OlympicService {
         // TODO: improve error handling
         console.error(error);
         // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
+        this.olympics$.next([]);
         return caught;
       })
     );
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,12 @@
 // home.component.ts
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import Olympic from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
-// Interface pour représenter la participation
-interface Participation {
-  id: number;
-  year: number;
-  city: string;
-  medalsCount: number;
-  athleteCount: number;
-}
-
-// Interface pour représenter un pays avec ses participations
-interface Country {
-  id: number;
-  country: string;
-  participations: Participation[];
+interface ChartValue{
+  name: string,
+  value: number,
 }
 
 @Component({
@@ -25,35 +15,31 @@ interface Country {
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<Country[]> = of([]);
+  public olympics$: Observable<Olympic[]> = of([]);
+
+  public chartValues$: Observable<ChartValue[]> = of([]);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     // Charger les données olympiques au moment de l'initialisation
     this.olympics$ = this.olympicService.getOlympics();
-  }
-
-  // Fonction pour filtrer les données et obtenir le total des médailles pour chaque pays
-  filterData(data: Country[]): any[] {
-    // Initialiser un tableau pour stocker les résultats
-    const result: any[] = [];
-    
-    // Parcourir chaque pays dans les données
-    data.forEach((country) => {
-      // Calculer le total des médailles pour le pays actuel
-      const totalMedals = country.participations.reduce((sum, participation) => {
-        return sum + participation.medalsCount;
-      }, 0);
-      
-      // Ajouter le résultat au tableau
-      result.push({
-        name: country.country,
-        value: totalMedals,
-      });
-    });
-
-    // Retourner le tableau de résultats
-    return result;
+    this.chartValues$ = this.olympicService.getOlympics().pipe(
+      map(countrys => {
+        const result: ChartValue[] = [];
+        countrys.forEach((country) => {
+          // Calculer le total des médailles pour le pays actuel
+          const totalMedals = country.participations.reduce((sum, participation) => {
+            return sum + participation.medalsCount;
+          }, 0);
+          // Ajouter le résultat au tableau
+        result.push({
+          name: country.country,
+          value: totalMedals,
+        });
+      })
+      // Retourner le tableau de résultats
+      return result
+    }));
   }
 }
