2a66dc2aca8bc205685fdde6fb74df8f1c2c433f
julien382 <julhermain@gmail.com>
2023-11-30 01:15:01 +0100

    clean detail

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
-import Olympic from 'src/app/core/models/Olympic';
+import { switchMap } from 'rxjs/operators';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
-// Interface pour définir la structure des valeurs de détail
 interface ChartValue {
   name: string;
   value: number;
@@ -20,11 +18,7 @@ interface ChartValue {
   styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
-  public olympics$: Observable<Olympic[]> = of([]);
-  public chartValues$: Observable<ChartValue[]> = of([]);
-  public numberEntries: number = 0;
-  public totalMedals: number = 0;
-  public totalAthletes: number = 0;
+  public chartValues: ChartValue[] = [];
 
   constructor(
     private olympicService: OlympicService,
@@ -32,48 +26,18 @@ export class DetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Extraire le paramètre de route pour obtenir le nom du pays
-    const countryName = this.route.snapshot.paramMap.get('country');
-
-    // Obtenir les données olympiques
-    this.olympics$ = this.olympicService.getOlympics();
-
-    // Utilisation de l'opérateur pipe pour manipuler les données olympiques
-    this.chartValues$ = this.olympics$.pipe(
-      map((countries) => {
-        // Filtrer les pays pour obtenir celui qui correspond au paramètre de la route
-        const result: ChartValue[] = countries
-          .filter((country) => country.country === countryName)
-          .map((country) => {
-            this.numberEntries = 0;
-
-            // Calcul du total des médailles pour le pays en cours
-            this.totalMedals = country.participations.reduce(
-              (sum, participation) => {
-                this.numberEntries++;
-                return sum + participation.medalsCount;
-              },
-              0
-            );
-
-            // Calcul du total des athlètes pour le pays en cours
-            this.totalAthletes = country.participations.reduce(
-              (sum, participation) => sum + participation.athleteCount,
-              0
-            );
-
-            // Création de l'objet ChartValue correspondant au pays en cours
-            return {
-              name: country.country,
-              value: this.totalMedals,
-              numberEntries: this.numberEntries,
-              medalsCount: this.totalMedals,
-              athleteCount: this.totalAthletes,
-            };
-          });
-
-        return result;
-      })
-    );
+    this.route.paramMap.pipe(
+      switchMap(params => this.olympicService.getOlympics().pipe(
+        map(pays => pays.find(p => p.country === params.get('country')))
+      ))
+    ).subscribe(selectedCountryData => {
+      this.chartValues = selectedCountryData ? [{
+        name: selectedCountryData.country,
+        value: selectedCountryData.participations.reduce((sum, participation) => sum + participation.medalsCount, 0),
+        numberEntries: selectedCountryData.participations.length,
+        medalsCount: selectedCountryData.participations.reduce((sum, participation) => sum + participation.medalsCount, 0),
+        athleteCount: selectedCountryData.participations.reduce((sum, participation) => sum + participation.athleteCount, 0),
+      }] : [];
+    });
   }
 }
