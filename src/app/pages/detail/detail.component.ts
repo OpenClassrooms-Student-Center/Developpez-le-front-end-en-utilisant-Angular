55afba8da36ef473ddb7cbc7e80715aed7d106ab
julien382 <julhermain@gmail.com>
2023-11-30 01:24:07 +0100

    comment detail

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -26,11 +26,17 @@ export class DetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // Utiliser le paramètre de la route pour obtenir le nom du pays
     this.route.paramMap.pipe(
-      switchMap(params => this.olympicService.getOlympics().pipe(
+      // Utiliser switchMap pour gérer les opérations asynchrones
+      switchMap(params => 
+        // Appeler olympicService pour obtenir la liste des pays
+        this.olympicService.getOlympics().pipe(
+          // Vérifier si le pays existe dans la liste
         map(pays => pays.find(p => p.country === params.get('country')))
       ))
     ).subscribe(selectedCountryData => {
+      // Affecter les valeurs du graphique en fonction des données du pays sélectionné
       this.chartValues = selectedCountryData ? [{
         name: selectedCountryData.country,
         value: selectedCountryData.participations.reduce((sum, participation) => sum + participation.medalsCount, 0),
