f2d0c526b21b2583c023f2715df30f3d3e1bcb6b
mel-a-lab <melanie.dussenne@gmail.com>
2023-12-01 16:39:04 +0100

    probleme recuperation detail country

--- src/app/pages/swimlane/swimlane.component.ts
+++ src/app/pages/swimlane/swimlane.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OlympicService } from '../../core/services/olympic.service';
+import { id } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-swimlane',
@@ -18,15 +19,20 @@ export class SwimlaneComponent {
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.countryName = params.get('countryName')!;
+      this.olympicService.getFormattedOlympics().subscribe(data => {
+        console.log(data);
+        const countryData = data.filter(country => country.id === 1)[0];
+        
+        if (countryData) {
+          this.participationsCount = countryData.participations.length;
+          this.totalMedals = countryData.participations.reduce((sum: any, participation: { medalsCount: any; }) => sum + participation.medalsCount, 0);
+          this.totalAthletes = countryData.participations.reduce((sum: any, participation: { athleteCount: any; }) => sum + participation.athleteCount, 0);
+          console.log(countryData);
+        }
+      });
+      
     });
-    this.olympicService.getFormattedOlympics().subscribe(data => {
-      const countryData = data.find(country => country.name === this.countryName);
-      if (countryData) {
-        this.participationsCount = countryData.participations.length;
-        this.totalMedals = countryData.participations.reduce((sum: any, participation: { medalsCount: any; }) => sum + participation.medalsCount, 0);
-        this.totalAthletes = countryData.participations.reduce((sum: any, participation: { athleteCount: any; }) => sum + participation.athleteCount, 0);
-      }
-    });
+    
 
   }
 
