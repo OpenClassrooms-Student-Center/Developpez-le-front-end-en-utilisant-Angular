27f526f99b2e21f9b821f6f6dd19af45ce855140
Patrice ALLARY <dev@allary-guillery.fr>
2023-12-18 18:17:28 +0100

    Ajout composant olympicDetails et implémentation de la route /details/{id} pour y acceder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {NgxChartsModule} from "@swimlane/ngx-charts";
+import { OlympicsDetailsComponent } from './components/olympics-details/olympics-details.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, DashboardComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, DashboardComponent, OlympicsDetailsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/olympics-details/olympics-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-olympics-details',
+  templateUrl: './olympics-details.component.html',
+  styleUrls: ['./olympics-details.component.scss']
+})
+export class OlympicsDetailsComponent {
+
+}
