9fba7a0bae5853df7c1f83d6b33ccb9bd7e67447
Patrice ALLARY <dev@allary-guillery.fr>
2023-12-19 21:11:56 +0100

    Implémentation Composant Detail

--- src/app/components/olympics-details/olympics-details.component.ts
+++ src/app/components/olympics-details/olympics-details.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {OlympicService} from "../../core/services/olympic.service";
+import {ActivatedRoute} from "@angular/router";
+import {filter, map, Subscription} from "rxjs";
+import {DetailsChartData} from "../../core/models/DetailsChartData";
+import {ChartData} from "../../core/models/ChartData";
+import {Olympic} from "../../core/models/Olympic";
 
 @Component({
   selector: 'app-olympics-details',
   templateUrl: './olympics-details.component.html',
   styleUrls: ['./olympics-details.component.scss']
 })
-export class OlympicsDetailsComponent {
+export class OlympicsDetailsComponent implements OnInit, OnDestroy{
+
+  olympicId !: number;
+  subscription !: Subscription;
+  datas !: DetailsChartData[];
+  countryName !: string;
+  entriesCount : number = 0;
+  medalsCount : number = 0;
+  athletesCount : number = 0;
+
+  /*
+  Variables ngx-chart
+   */
+  view : [number, number] = [640, 480];
+  showLegend : boolean = false;
+  xAxis : boolean = true;
+  yAxis : boolean = false;
+  showXAxisLabel : boolean = true;
+  showYAxisLabel : boolean = true;
+  xAxisLabel : string = "Year";
+  yAxisLabel : string = "Medals";
+  autoScale : boolean = true;
+
+  constructor(private olympicService : OlympicService, private route : ActivatedRoute) {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.olympicId = +<string>this.route.snapshot.paramMap.get('id');
+    this.subscription = this.olympicService.getOlympics().subscribe(olympics => {
+      this.datas = this.mapDatas(olympics);
+    });
+  }
+
+  mapDatas(olympics : Olympic[]) : DetailsChartData[] {
+    let results: DetailsChartData[] = [];
+    for (let olympic of olympics) {
+      if (olympic.id === this.olympicId) {
+        let medalResults : DetailsChartData = new DetailsChartData("medals");
+        this.entriesCount = olympic.participations.length;
+        this.countryName = olympic.country;
+        for (let participation of olympic.participations) {
+          this.athletesCount += participation.athleteCount;
+          this.medalsCount += participation.medalsCount;
+          medalResults.series.push(new ChartData(participation.id, participation.year.toString(), participation.medalsCount));        }
+        results.push(medalResults);
+      }
+    }
+    return results;
+  }
+
+
 
 }
