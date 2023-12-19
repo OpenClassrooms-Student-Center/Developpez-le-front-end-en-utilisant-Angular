f3c8d471084f6e0c15b68f9e347b8c81310802ea
Zakariya <z.bazaou@outlook.fr>
2023-12-19 11:47:57 +0100

    feature(projet2):ngx pie charts page principal + bootstrap

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,11 +8,40 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<any> = this.olympicService.getOlympics();
+  public pieChartData: any[] = [];
+  public numberOfCountries : any = 0;
+  public numberOfOlympics : any = 0;
+
+  public colorScheme: any = {
+    name: 'custom',
+    selectable: true,
+    group: 'Ordinal',
+    domain: ['#793D52', '#B8CBE7', '#89A1DB', '#793D52', '#FFA07A'],
+  };
+
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-  }
+   
+      this.olympics$.subscribe((olympics) => {
+         
+        this.pieChartData = olympics.map((country:any) => {
+          return {
+            name: country.country,
+            value: country.participations.reduce(
+              (totalMedals:any, participation:any) => totalMedals + participation.medalsCount,
+              0
+            ),
+          };
+        });
+        this.numberOfOlympics = olympics.reduce(
+          (totalOlympics: any, country: any) => totalOlympics = country.participations.length,
+          0
+        );
+         this.numberOfCountries = olympics.length;
+      });
+     
+    }
 }
