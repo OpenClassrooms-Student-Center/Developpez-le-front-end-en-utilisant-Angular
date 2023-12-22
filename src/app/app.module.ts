d5b28317f59e64e386e66a983f59296366051e95
Gerald P <thegege2005@yahoo.fr>
2023-12-22 17:35:01 +0100

    Add detail component + update color of caret for tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, DashboardComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, DashboardComponent, DetailComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule,NgxChartsModule],
   providers: [],
   bootstrap: [AppComponent],
