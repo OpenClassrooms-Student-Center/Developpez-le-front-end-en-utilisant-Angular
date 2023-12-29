bdbe2b2b619cb65c027b394c2a582fcd4a64f434
Patrice ALLARY <dev@allary-guillery.fr>
2023-12-29 12:19:58 +0100

    Creation of GlobalErrorHandler Class and register it in AppModule as a ErrorHandler Provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import {ErrorHandler, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,11 +9,17 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {NgxChartsModule} from "@swimlane/ngx-charts";
 import { OlympicsDetailsComponent } from './components/olympics-details/olympics-details.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {GlobalErrorHandler} from "./core/services/global-error-handler";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, DashboardComponent, OlympicsDetailsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule, BrowserAnimationsModule],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
