01a14b249eb02b2d9a3a8ef449d0146551ef4460
Meceline <celinemercadier@gmail.com>
2024-01-29 11:18:25 +0100

    fix: display message when data is empty,

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -26,33 +26,34 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subscription = this.olympicService.getOlympics().subscribe({
       next: (data) => {
-      this.olympics = data;
-      this.numberOfCountries = this.olympics.length;
-
-      const uniqueCities = new Set<string>(); 
-      this.olympics.forEach((country) => {
-        country.participations?.forEach((participation: Participation) => {
-          if (participation.city !== undefined) {
-            uniqueCities.add(participation.city);
-          }
-        });
+        this.olympics = data;
+        if(this.numberOfCountries ===  null) this.message = "Pas de résultats"
+        else{
+          this.numberOfCountries = this.olympics.length;
+          const uniqueCities = new Set<string>(); 
+          this.olympics.forEach((country) => {
+            country.participations?.forEach((participation: Participation) => {
+              if (participation.city !== undefined) {
+                uniqueCities.add(participation.city);
+              }
+            });
+          });
+          // Récupérer le nombre de villes distinctes
+          this.numberOfCities = uniqueCities.size;
+          //Prepare data pour graphique
+          this.chartData = this.olympics.map(country => ({
+            id: country.id,
+            name: country.country ?? '',
+            value: country.participations?.reduce((total, p) => total + (p.medalsCount ?? 0), 0)
+          }));
+          this.messageVisible = false
+        }   
+      },
+      error: (error) => {
+        this.message = error;
+        this.messageVisible = true
+        }
       });
-      // Récupérer le nombre de villes distinctes
-      this.numberOfCities = uniqueCities.size;
-
-      //Prepare data pour graphique
-      this.chartData = this.olympics.map(country => ({
-        id: country.id,
-        name: country.country ?? '',
-        value: country.participations?.reduce((total, p) => total + (p.medalsCount ?? 0), 0)
-      }));
-      this.messageVisible = false
-     },
-     error: (error) => {
-			this.message = error;
-			this.messageVisible = true
-		  }
-    });
   }
 
   ngOnDestroy(): void {
