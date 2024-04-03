0dac1972972992d57d3b5dda09eede9981b83d20
Denizot <denizot.myriam@gmail.com>
2024-04-03 21:55:03 +0200

    any  on olympic service and semantic html

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+
+interface Olympic {
+  // Définissez les propriétés de l'interface Olympic en fonction des données à récupérer
+  // Exemple :
+  id: number;
+  name: string;
+  year: number;
+  hostCity: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[]>([]); // BehaviorSubject contenant un tableau d'objets Olympic
 
   constructor(private http: HttpClient) {}
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+  loadInitialData(): Observable<Olympic[]> { // Renvoie un Observable qui émet un tableau d'objets Olympic
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
         console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
+        this.olympics$.next([]); // En cas d'erreur, émet un tableau vide pour signaler un problème
         return caught;
       })
     );
   }
 
-  getOlympics() {
+  getOlympics(): Observable<Olympic[]> { // Renvoie un Observable représentant la valeur courante du BehaviorSubject
     return this.olympics$.asObservable();
   }
 }
