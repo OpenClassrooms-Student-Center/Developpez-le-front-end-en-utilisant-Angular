08b2caa241355386971d86bed080d39e9f6c8df1
othAs-dev <othmane.stig@gmail.com>
2024-05-06 18:41:10 +0200

    wip

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
-import {Component} from '@angular/core';
-import {Observable, tap} from 'rxjs';
-import {AsyncPipe, NgIf} from "@angular/common";
+import {Component, inject} from '@angular/core';
+import {map, tap} from 'rxjs';
+import {AsyncPipe} from "@angular/common";
 import {OlympicService} from "@services/olympic.service";
 import {Olympics} from "@models/Olympic";
 import {TitleComponent} from "@shared/components/title/title.component";
 import {TagsComponent} from "@shared/components/tags/tags.component";
+import {NgxChartsModule} from "@swimlane/ngx-charts";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -15,20 +17,37 @@ import {TagsComponent} from "@shared/components/tags/tags.component";
     AsyncPipe,
     TitleComponent,
     TagsComponent,
-    NgIf
+    NgxChartsModule,
   ]
 })
 export default class HomeComponent {
-  private _olympicService: OlympicService = new OlympicService();
+  private _olympicService: OlympicService = inject(OlympicService);
+  private _router: Router = inject(Router);
 
   protected numberOfCountry!: number;
-  protected numberOfMedals!: number;
+  protected numberOfParticipations!: number;
 
-  protected olympics$: Observable<Olympics> = this._olympicService.getOlympics$().pipe(
+  protected olympics$ = this._olympicService.getOlympics$().pipe(
     tap((olympics: Olympics) => {
-      this.numberOfMedals = olympics.flatMap(o => o.participations)
-        .reduce((acc, participation) => acc + participation.medalsCount, 0);
+      this.numberOfParticipations = olympics.flatMap(o => o.participations).length;
       this.numberOfCountry = olympics.length
-    })
-  );
+    }),
+    map((olympics: Olympics) => {
+        return olympics.map(olympic => {
+          return {
+            id: olympic.id,
+            name: olympic.country,
+            value: olympic.participations.reduce((acc, participation) => acc + participation.medalsCount, 0)
+          }
+        })
+      }
+    ));
+
+  navigateToDetail(data: any) {
+    console.log(data)
+    this.olympics$.pipe(
+      map(olympics => olympics.find(olympic => olympic.name === data.name)),
+      tap(olympic => this._router.navigate(['', olympic!.id])
+      )).subscribe()
+  }
 }
