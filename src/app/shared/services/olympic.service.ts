75d5d9c0c0dd05746c4a25a4f6bcb6aa9215aa3e
othAs-dev <othmane.stig@gmail.com>
2024-05-10 10:25:53 +0200

    adding comments to the ngOnInit() method in the detail country component and adjusting of the template to no longer have the error message displayed and improvement of the service to get less code

--- src/app/shared/services/olympic.service.ts
+++ src/app/shared/services/olympic.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {BehaviorSubject, catchError, EMPTY, map, Observable, tap} from "rxjs";
+import {catchError, EMPTY, map, Observable} from "rxjs";
 import {Olympic, Olympics} from "@models/Olympic";
 import {Id} from "@models/id";
 
@@ -9,13 +9,10 @@ import {Id} from "@models/id";
 })
 export class OlympicService {
  private _http = inject(HttpClient);
-  private olympicUrl = './assets/mock/olympic.json';
-
-  private _olympics$ = new BehaviorSubject<Olympics>([]);
+  private _olympicUrl = './assets/mock/olympic.json';
 
   public getOlympics$(): Observable<Olympics> {
-    return this._http.get<Olympics>(this.olympicUrl).pipe(
-      tap((olympics) => this._olympics$.next(olympics)),
+    return this._http.get<Olympics>(this._olympicUrl).pipe(
       catchError((error) => {
         console.error('Error fetching olympics', error);
         return EMPTY;
@@ -24,10 +21,8 @@ export class OlympicService {
   }
 
   public getOlympic$(id: Id): Observable<Olympic | undefined> {
-    return this._http.get<Olympics>(this.olympicUrl).pipe(
-      map((olympics) => {
-         return olympics.find((olympic) => olympic.id === id);
-      }),
+    return this._http.get<Olympics>(this._olympicUrl).pipe(
+      map((olympics) => olympics.find((olympic) => olympic.id === id)),
       catchError((error) => {
         console.error('Error fetching olympic', error);
         return EMPTY;
