0e7536b54cea92e919f578fbde007731efdd0309
othAs-dev <othmane.stig@gmail.com>
2024-05-17 11:29:29 +0200

    adding tsdoc to method installing angular/cdk dependency and finally adding a return void to navigateToDetail method

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import {Component, inject} from '@angular/core';
 import {map, Observable, take, tap} from 'rxjs';
 import {AsyncPipe} from "@angular/common";
 import {OlympicService} from "@services/olympic.service";
-import {Olympic, Olympics} from "@models/Olympic";
+import {Olympics} from "@models/Olympic";
 import {TitleComponent} from "@shared/components/title/title.component";
 import {TagsComponent} from "@shared/components/tags/tags.component";
 import {NgxChartsModule} from "@swimlane/ngx-charts";
@@ -28,6 +28,19 @@ export default class HomeComponent {
   protected numberOfCountry!: number;
   protected numberOfParticipations!: number;
 
+  /**
+   * Observable of OlympicsLineChart
+   *
+   * This observable is used to generate the data for the line chart.
+   *
+   * @type {Observable<OlympicsLineChart>}
+   * @memberof HomeComponent
+   *
+   * return {Observable<OlympicsLineChart>} The observable olympics$ returns an array of objects, where each object has the following properties:
+   *  - id: The ID of the country.
+   *  - name: The name of the country.
+   *  - value: The total number of medals won by the country.
+   */
   protected olympics$: Observable<OlympicsLineChart> = this._olympicService.getOlympics$().pipe(
     tap((olympics: Olympics) => {
       this.numberOfParticipations = olympics.flatMap(o => o.participations).length;
@@ -44,7 +57,19 @@ export default class HomeComponent {
       }
     ));
 
-  navigateToDetail(data: {name: string, value: number, label: string}) {
+  /**
+   * Navigate to detail
+   * @param data object with name, value and label properties
+   * @memberof HomeComponent
+   *
+   * This method is used to navigate to the detail page of the selected country.
+   *
+   * @returns {void}
+   *
+   * Important information: This method has a observable with a take(1) operator,
+   * which means that it will only take the first value emitted by the observable.
+   */
+  protected navigateToDetail(data: {name: string, value: number, label: string}): void {
     this.olympics$.pipe(
       take(1),
       map(olympics => olympics.find(olympic => olympic.name === data.name)),
--- src/app/shared/services/olympic.service.ts
+++ src/app/shared/services/olympic.service.ts
@@ -8,9 +8,27 @@ import {Id} from "@models/id";
   providedIn: 'root',
 })
 export class OlympicService {
- private _http = inject(HttpClient);
+  private _http = inject(HttpClient);
   private _olympicUrl = './assets/mock/olympic.json';
 
+  /**
+   * Get the list of Olympics
+   * @memberof OlympicService
+   * This method fetches the list of Olympics from the server.
+   *
+   * @returns {Observable<Olympics>} The observable olympics$ returns an array of objects, where each object has the following properties:
+   * - id: The ID of the country.
+   * - country: The name of the country.
+   * - participations: An array of objects, where each object has the following properties:
+   *  * - id: The ID of the Olympic event.
+   * - year: The year of the Olympic event.
+   * - city: The city of the Olympic event.
+   * - medalsCount: The number of medals won by the country in the Olympic event.
+   * - athleteCount: The number of athletes representing the country in the Olympic event.
+   *
+   * Finally, the method catches any errors and logs them to the console.
+   * And returns an empty array if there are no Olympics.
+   */
   public getOlympics$(): Observable<Olympics> {
     return this._http.get<Olympics>(this._olympicUrl).pipe(
       catchError((error) => {
@@ -20,6 +38,27 @@ export class OlympicService {
     );
   }
 
+  /**
+   * Get the details of an Olympic event
+   * @memberof OlympicService
+   * @param {Id} id The ID of the Olympic event.
+   *
+   * This method fetches the details of an Olympic event from the server.
+   *
+   * @returns {Observable<Olympic | undefined>} The observable olympic$ returns an object with the following properties:
+   * - id: The ID of the country.
+   * - country: The name of the country.
+   * - participations: An array of objects, where each object has the following properties:
+   * * - id: The ID of the Olympic event.
+   * - year: The year of the Olympic event.
+   * - city: The city of the Olympic event.
+   * - medalsCount: The number of medals won by the country in the Olympic event.
+   * - athleteCount: The number of athletes representing the country in the Olympic event.
+   *
+   * And returns undefined if the Olympic event is not found.
+   *
+   * Finally, the method catches any errors and logs them to the console.
+   */
   public getOlympic$(id: Id): Observable<Olympic | undefined> {
     return this._http.get<Olympics>(this._olympicUrl).pipe(
       map((olympics) => olympics.find((olympic) => olympic.id === id)),
