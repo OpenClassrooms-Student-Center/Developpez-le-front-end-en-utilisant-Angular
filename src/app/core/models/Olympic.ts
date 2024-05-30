4eb4eda30994fa255492eeca2f236cea7f878539
Noémie <baudry.ddf@gmail.com>
2024-05-30 17:31:48 +0200

    annulation des update

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -7,3 +7,16 @@ example of an olympic country:
     participations: []
 }
 */
+import { Participation } from "./Participation";
+
+export class Olympic {
+    public id?: number;
+    public country?: string;
+    public participations?: Participation[];
+
+    constructor(id: number, country: string, participations: Participation[]) {
+        this.id = id;
+        this.country = country;
+        this.participations = participations;
+    }
+}
--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -9,3 +9,18 @@ example of participation:
     athleteCount: 372
 }
 */
+export class Participation {
+    public id: number;
+    public year: number;
+    public city: string;
+    public medalsCount: number;
+    public athleteCount: number;
+
+    constructor(id: number, year: number, city: string, medalsCount: number, athleteCount: number) {
+        this.id = id;
+        this.year = year;
+        this.city = city;
+        this.medalsCount = medalsCount;
+        this.athleteCount = athleteCount;
+    }
+}
