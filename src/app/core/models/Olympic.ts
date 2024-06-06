c67eee87ee96e6623122a9a4fef0b4d5d888a44c
Romain Boinot <phantomatik@hotmail.fr>
2024-06-06 12:59:23 +0200

    Added Data Types for countries and participations

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,9 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
+import { Participation } from "./Participation";
+
+export class Olympic
 {
-    id: 1,
-    country: "Italy",
-    participations: []
+    constructor(public id: number,
+                public country: string,
+                public participations?: Participation[]) 
+    {}
 }
-*/
--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -1,11 +1,9 @@
-// TODO: create here a typescript interface for a participation
-/*
-example of participation:
+export class Participation 
 {
-    id: 1,
-    year: 2012,
-    city: "Londres",
-    medalsCount: 28,
-    athleteCount: 372
+    constructor(public id: number,
+                public year: number,
+                public city: string,
+                public medalsCount: number,
+                public athleteCount: number)
+    {}
 }
-*/
