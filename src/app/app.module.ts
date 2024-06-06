f528bb9cabca612900e2686f77061559dbb9cd39
KHALFALLAH Razzak (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDA TP DOI) <9506532m@commun.ad.sncf.fr>
2024-06-06 02:55:24 +0200

    feat/pdw-etape3: creation of the first graph without details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-
+import { NgChartsModule } from 'ng2-charts';
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData } from 'chart.js';
 import { Observable, of } from 'rxjs';
+import { Country } from 'src/app/core/models/Olympic';
+import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -8,11 +11,63 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  private country: Country[] = [];
+  public barChartData: ChartData<"pie", number[]> | undefined;
 
-  constructor(private olympicService: OlympicService) {}
+
+  public barChartOptions = {
+
+  }
+  public error: Error | undefined;
+
+  constructor(private olympicService: OlympicService) { }
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.getAllCountrysAndInitializeGraph();
+  }
+
+  /**
+   * get countrys with their datas and launch method that initialize barChartData and barChartOptions
+   */
+  getAllCountrysAndInitializeGraph(): void {
+    this.olympicService.getOlympics().subscribe({
+      next: (country: Country[]) => { this.country = country; this.initializeBarChartData(this.country) },
+      error: (error: Error) => this.error = error
+    })
+
+  }
+
+  /**
+   *
+   * @param country
+   */
+  initializeBarChartData(country: Country[]) {
+    this.barChartData = {
+      datasets: [{
+        data: this.getAllMedalsForEachCountry(country)
+      }],
+      labels: this.getAllLabelsForEachCountry(country)
+    }
+  }
+
+  /**
+   *
+   * @param country
+   * @returns an array that contains the sum of each medals per participation of each country
+   */
+  getAllMedalsForEachCountry(country: Country[]): number[] {
+    return [...country
+      .map((oneCountry: Country) => oneCountry.participations
+        .map((participation: Participation) => participation.medalsCount)
+        .reduce((acc, currentValue) => acc + currentValue))]
+  }
+
+  /**
+   *
+   * @param country
+   * @returns an array that contains all country labels
+   */
+  getAllLabelsForEachCountry(country : Country[]) : string[]{
+    return [...country.map((country : Country) => country.country)]
   }
 }
