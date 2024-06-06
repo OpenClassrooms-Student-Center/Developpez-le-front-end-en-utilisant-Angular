ba3a25b03092024ba4c09b27120f792002f78da8
Romain Boinot <phantomatik@hotmail.fr>
2024-06-06 15:24:11 +0200

    updated Home Component to use added data types

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Olympic } from 'src/app/core/models/Olympic';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<Olympic[]> = of([]);
 
   constructor(private olympicService: OlympicService) {}
 
