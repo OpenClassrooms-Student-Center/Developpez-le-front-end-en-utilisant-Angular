ef3c8e1e7ddb3eff2a26729da542499aa038be04
Romain Boinot <phantomatik@hotmail.fr>
2024-06-07 16:56:14 +0200

    Fed our data to the pie chart, also added number of JOs and number of countries

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,37 +2,61 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Olympic } from 'src/app/core/models/Olympic';
-import { LegendPosition, NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  public olympics$: Observable<Olympic[]> = of([]);
+export class HomeComponent implements OnInit 
+{
+  olympics$: Observable<Olympic[]> = of([]);
+  chartData: {city : String, year : Number}[] = [];
 
-  //
+  JOsCount: Number = 0;
+  countriesCount: Number = 0;
+
+  // Chart parameters
   gradient: boolean = true;
   showLegend: boolean = true;
   showLabels: boolean = true;
   isDoughnut: boolean = false;
-
-  data = [
-    {
-      "name": "Germany",
-      "value": 8940000
-    },
-    {
-      "name": "USA",
-      "value": 5000000
-    }
-  ]
-  //
+  colorScheme: any = {domain: ['#956065', '#793D52', '#89A1DB', '#9780A1', '#BFE0F1', '#B8CBE7']}
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
+    // Fetch data and subscribe for changes
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.subscribe(data => this.onOlympicsUpdate(data));
+  }
+
+  // Parse Olympics to be used with the pie chart
+  private formatOlympics(olympics : Olympic[]) : any{
+    return olympics.map(olympic => ({
+        name: olympic.country,
+        value: olympic.participations?.reduce((sum: number, p: { medalsCount: number }) => sum + p.medalsCount, 0),
+        participations: olympic.participations, 
+      }));
+  }
+
+  // Called whenever our data changed
+  private onOlympicsUpdate(data : Olympic[]) {
+    this.chartData = this.formatOlympics(data);
+    this.countriesCount = data.length;
+
+    // Calculate number of JOs
+    let jos : { city : String, year : Number}[] = [];
+    data.forEach(olympic => {
+      if (olympic.participations)
+      {
+        olympic.participations.forEach(participation => {
+          let b = jos.find((jo) => jo.city === participation.city && jo.year === participation.year)
+          if (b === undefined)
+            jos.push({ city : participation.city, year : participation.year});
+        })
+      }
+    });
+    this.JOsCount = jos.length;
   }
 }
